c881ae9f5b95826377476bfd8a5ffaa69de3b852
abhijitmondal121 <mondalavi9836@gmail.com>
2021-07-27 13:13:54 +0530

    init

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,3 +1,16 @@
-<div class="container-fluid bg-dark text-light mt-0">
-<p class="text-center mb-0 py-1">Copyright i-Quiz 2021 || all reserve</p>
+
+<div class="x" style="background:black;padding:30px 0px; margin-top:0px;">
+<center><h3 style="font-size:50px;">I-Quiz</h1></center>
+
+<div class="ro  "style="display:flex;justify-content: center;">
+
+    <div class="co my-2"><img src="partials/fac1.png" alt="" style="height:50px;width:50px;"></div>
+    <div class="co my-2"><img src="partials/t.png" alt="" style="height:50px;width:50px;margin-left:20px;"></div>
+    <div class="co my-2"><img src="partials/lin.png" alt="" style="height:50px;width:50px;margin-left:20px;"></div>
+    <div class="co my-2"><img src="partials/insta.png" alt="" style="height:51px;width:55px;margin-left:20px;"></div>
+    
+</div>
+
+<p class="text-center mb-0 mt-4"style="font-size:18px;"><b>Copyright i-Quiz 2021 || all reserve</b></p>
+
 </div>
